5128d5ff4d3ba0a0ee619fa58df332238067dd70
sarahpappas <pappas.sarah@gmail.com>
2016-09-22 10:52:56 -0700

    styling adjusted

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ console.log("JS file is linked up!");
 
 $(document).ready(function(){
 	// get variables
-	var startButton = $("#start-button");
+	var startButton = $(".start");
 	var nextButton = $("#next-button");
 	var quizArea = $(".quiz");
 	var choiceArea = $(".choices");
@@ -14,14 +14,14 @@ $(document).ready(function(){
 	var quizWrapper = $(".quiz-wrapper");
 	var allQuestions = [
 		{question: "Nobody puts Baby in a corner.", choices: ["Dirty Dancing", "Three Men and a Baby", "Pretty Woman"], correctAnswer:0},
-		{question: "I ate his liver with some fava beans and a nice Chianti.", choices: ["Silence of the Lambs", "Psycho", "Friday the 13th"], correctAnswer:0},
-		{question: "My name is Inigo Montoya. You killed my father. Prepare to die", choices: ["Gone with the Wind", "Fargo", "The Princess Bride"], correctAnswer:2},
-		{question: "I drink your milkshake!", choices: ["Animal House", "There Will Be Blood", "Back to the Future"], correctAnswer:1},
-		{question: "Say hello to my little friend!", choices: ["The Godfather", "Donnie Brasco", "Scarface"], correctAnswer:2},
-		{question: "Show me the MONEY!", choices: ["Jerry Maguire", "Titanic", "Casino"], correctAnswer:0},
-		{question: "I see dead people.", choices: ["Sixth Sense", "A Nightmare on Elm Street", "Hellraiser"], correctAnswer:0},
-		{question: "You're gonna need a bigger boat.", choices: ["The Perfect Storm", "Jaws", "Captain Ron"], correctAnswer:1},
-		{question: "I'm gonna make him an offer he can't refuse.", choices: ["Glengarry Glen Ross", "The Wolf of Wall Street","The Godfather"], correctAnswer:2}
+		// {question: "I ate his liver with some fava beans and a nice Chianti.", choices: ["Silence of the Lambs", "Psycho", "Friday the 13th"], correctAnswer:0},
+		// {question: "My name is Inigo Montoya. You killed my father. Prepare to die", choices: ["Gone with the Wind", "Fargo", "The Princess Bride"], correctAnswer:2},
+		// {question: "I drink your milkshake!", choices: ["Animal House", "There Will Be Blood", "Back to the Future"], correctAnswer:1},
+		// {question: "Say hello to my little friend!", choices: ["The Godfather", "Donnie Brasco", "Scarface"], correctAnswer:2},
+		// {question: "Show me the MONEY!", choices: ["Jerry Maguire", "Titanic", "Casino"], correctAnswer:0},
+		// {question: "I see dead people.", choices: ["Sixth Sense", "A Nightmare on Elm Street", "Hellraiser"], correctAnswer:0},
+		// {question: "You're gonna need a bigger boat.", choices: ["The Perfect Storm", "Jaws", "Captain Ron"], correctAnswer:1},
+		// {question: "I'm gonna make him an offer he can't refuse.", choices: ["Glengarry Glen Ross", "The Wolf of Wall Street","The Godfather"], correctAnswer:2}
 	];
 	var score = 0;
 	var questionIndex = 0;
@@ -59,7 +59,6 @@ $(document).ready(function(){
 			};
 			//increase index
 			questionIndex++;
-			console.log(score);
 			// empty the choice element
 			choiceArea.empty();
 			//empty the question element
@@ -71,14 +70,15 @@ $(document).ready(function(){
 			insertEl(questionEl, "prepend", questionDisplay);
 			// CHANGE to a function
 			allQuestions[questionIndex].choices.forEach(function(choice){
-				console.log(choiceArea);
 				var choiceDisplay = "<div class='input'>" + "<input type='radio' name='choice' value='" + choice + "'><p>" + choice + "</p></div>";
 				insertEl(choiceArea, "append", choiceDisplay)
 			});
 			} else {
-				var textDisplay = "<div class='score'> You scored " + score + " out of " + allQuestions.length + "</div>";
+				var textDisplay = "<div class='score'> You scored " + score + " out of " + allQuestions.length + ".</div>";
 				insertEl(questionEl, "append", textDisplay);
+				insertEl(questionEl, "append", "<img class='main-image' src='img/2001.jpg'>")
 				changeElDisplay(nextButton, "none");
+				$(".choices").remove();
 			}
 
 			// if statement to see if last question and if so show score if not, do above
