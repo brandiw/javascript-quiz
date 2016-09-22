f79d7798a2df5d2f02f7d8eb27f82cc5d7ed9c2b
sarahpappas <pappas.sarah@gmail.com>
2016-09-22 08:58:29 -0700

    fixed issue

--- js/index.js
+++ js/index.js
@@ -23,33 +23,34 @@ $(document).ready(function(){
 		{question: "I'm gonna make him an offer he can't refuse.", choices: ["Glengarry Glen Ross", "The Wolf of Wall Street","The Godfather"], correctAnswer:2}
 	];
 	var score = 0;
-	var questionIndex = 1;
+	var questionIndex = 0;
 
 	startButton.click(function() {
 		startButton.css("display", "none");
 		//display quiz wrapper
 		$(".quiz-wrapper").css("display", "initial");
 		// Display first quesiton
-		questionEl.prepend("<div>" + allQuestions[0].question + "</div>");
-		allQuestions[0].choices.forEach(function(choice){
+		questionEl.prepend("<div>" + allQuestions[questionIndex].question + "</div>");
+		allQuestions[questionIndex].choices.forEach(function(choice){
 			console.log(choiceArea);
 			choiceArea.append("<input type='radio' name='choice' value='" + choice + "'>" + choice);
 		});
+
 	});
 
 	nextButton.click(function() {
 		// get value of selected radio button
-		console.log();
-		var choices = allQuestions[questionIndex].choices;
 		var userInput = $("input[name='choice']:checked").val();
-		var userInputIndex = choices.indexOf(userInput);
+		var choicesArray = allQuestions[questionIndex].choices;
+		console.log(userInput);
+		var userInputIndex = choicesArray.indexOf(userInput);
 		console.log("user input " + userInputIndex);
 		// Need to - check for correctnes, if correct add point to score
 		var correctAnswerNum = allQuestions[questionIndex].correctAnswer;
 		//allQuestions[questionIndex].choices[correctAnswerNum] == correctAnswerNum
 		var correctChoicesIndex = allQuestions[questionIndex].correctAnswer;
-		console.log("correct asnwer num " + choices.indexOf(choices[correctAnswerNum]));
-		if (choices.indexOf(choices[userInputIndex]) == correctAnswerNum) {
+		console.log("correct asnwer num " + choicesArray.indexOf(choicesArray[correctAnswerNum]));
+		if (choicesArray.indexOf(choicesArray[userInputIndex]) == correctAnswerNum) {
 		    score++;
 		};
 		//increase index
@@ -69,7 +70,8 @@ $(document).ready(function(){
 			choiceArea.append("<input type='radio' name='choice' value='" + choice + "'>" + choice);
 		});
 		} else {
-			questionEl.append("<div class='score'>" + score + "</div>");
+			questionEl.append("<div class='score'> You scored " + score + " out of " + allQuestions.length + "</div>");
+			nextButton.css("display", "none");
 		}
 
 		// if statement to see if last question and if so show score if not, do above
