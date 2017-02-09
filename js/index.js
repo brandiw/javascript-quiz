3f2c35f24a6016124b9ca35c064ac1926ea181eb
themartinsjoseph <themartinsjoseph@gmail.com>
2017-02-09 15:01:27 -0800

    created a boilerplate and added JS

--- js/index.js
+++ js/index.js
@@ -2,4 +2,15 @@
 * Your names
 */
 
-console.log("JS file is linked up!");
+var allQuestions = [{question: "Who is the Mayor of Seattle?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswerIndex:0}, {question: "who"}]; 
+
+console.log(allQuestions[0].question); 
+console.log(allQuestions[0].choices);
+console.log(allQuestions[0].correctAnswerIndex);
+
+$("#submit").click("submitQuiz()");
+
+
+	function submitQuiz () {
+		console.log("FIRING?");
+	};
