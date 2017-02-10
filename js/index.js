15e5e8db0ca88d5ea96daa79bafde7bd8d7ee4aa
jnhustin <jnhustin@gmail.com>
2017-02-09 18:33:57 -0800

    added function checkEndgame, note it's incomplete

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ var allQuestions = [{question:"Complete the following Mark Twain quote: 'Go to H
 					{question:"What poker hand did Wild Bill Hickok have when he was shot in a Deadwood saloon?", choices: ['Full House (Queens over fives)','2 pair (Aces and eights)', 'Flush (Hearts to the ten)', 'Three of a Kind (Sevens)'], correctAnswerIndex:1},
 					{question:"What is the last word in the Declaration of Independence?", choices: ['Honor', 'Fortunes', 'Lives', 'Republic'], correctAnswerIndex:0},
 					{question:"What is Canada's most populous city?", choices: ['Montreal','Quebec City', 'Winnipeg', 'Toronto'], correctAnswerIndex:3}]
-var questionNumber = 0;
+var questionNumber = 3;
 var score;
 var time;
 
@@ -20,6 +20,9 @@ $('#lab2').html(allQuestions[questionNumber].choices[1]);
 $('#lab3').html(allQuestions[questionNumber].choices[2]);
 $('#lab4').html(allQuestions[questionNumber].choices[3]);
 
+timer();
+
+
 
 $('#nextButton').on('click', function() {
 	checkAnswer();
@@ -29,17 +32,33 @@ $('#nextButton').on('click', function() {
 	if (questionNumber === allQuestions.length) {
 		switchToSubmitButton()
 	}
+});
 
-
-});;
-timer()
 function timer() {
-	time = 30;
+	time = 5;
 	var tickDown = setInterval(function(){
 		time -= 1;
 		$('#timeDisplay').text(time.toString());
+
+		if (time === 0) {
+			console.log('next question will now commence'); //placeholder
+			clearInterval(tickDown);
+			
+			checkEndgame();
+		}
 	}, 1000)
 }
+function checkEndgame() {
+	if (questionNumber === allQuestions.length) {
+		checkAnswer();
+		clearInterval(tickDown);
+		tallyScore();
+	} else {
+		questionNumber += 1;
+		nextQuestion();
+		timer();
+	}
+}			
 function checkAnswer(){
 	
 };
@@ -54,9 +73,6 @@ function nextQuestion(){
 	$('#lab3').html(allQuestions[questionNumber].choices[2]);
 	$('#lab4').html(allQuestions[questionNumber].choices[3]);
 	$('#question').html(allQuestions[questionNumber].question);
-setTimeout(function(){ 
-	alert("Hello"); 
-}, 30000);
 };
 
 function switchToSubmitButton(){
