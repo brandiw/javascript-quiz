19b2acbd7742a47455dbfe8d6c0b818ef4dcac9b
regbake <regbake@gmail.com>
2017-09-07 16:53:05 -0700

    first commit, added up the style, js and html content

--- js/script.js
+++ js/script.js
@@ -1,5 +1,50 @@
-/*
-* Your names
-*/
+var allQuestions = [
+{
+	question: "What makes a muskrat guard hus musk?",
+	choices: ["Flair", "GREAT SCOTT", "Courage", "Rosebud"],
+	correctAnswerIndex: 2
+	},
+	{
+	question: "Whooo aaarrrreeee yooooouuuuu?",
+	choices: [“Owl”, “Alice”, “Mr Bean”, “The Great and Powerful Oz”],
+	correctAnswerIndex: 2
+	},
+	{
+	question: "First person to single-hand around the world?",
+	choices: ["Joshua Slocum", "Steven Jackson", "Tristan Jones", "Joe Momma"],
+	correctAnswerIndex: 0
+	},
+	{
+	question: "Most common hull material of sailing yachts",
+	choices: ["Wood", "Copper", "Cement", "Fiber Reinforced Plastic"],
+	correctAnswerIndex: 3
+	},
+	{
+	question: "Favorite beer of Petaluma, CA",
+	choices: ["Fieldwork", "Sierra Nevada", "Lagunitas", "Rainier"],
+	correctAnswerIndex: 2
+	},
+	{
+	question: "Chlorophyll, more like...? ",
+	choices: ["plants", "Borophyll", "Chloroplasts", "greenie"],
+	correctAnswerIndex: 2
+	},
+	{
+	question: "If peeing in your pants is cool, consider me...?",
+	choices: ["Barack Obama", "Steven Jackson", "James Brown", "Miles Davis"],
+	correctAnswerIndex: 3
+	},
+	{
+	question: "O'Doyle...?",
+	choices: ["Rules", "Kicks Butt", "Wins", "Cools"],
+	correctAnswerIndex: 0
+	},
+	{
+	question: "",
+	choices: ["", "", "", ""],
+	correctAnswerIndex: 
+}];
 
-console.log("JS file is linked up!");
+function currentQuestion() {
+	//var questionNumber = 0;
+}
