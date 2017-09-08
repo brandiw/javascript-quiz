946be26c2b9ea51b2846cdef64b6f9964efee0be
jbadan <jenna.badanowski@gmail.com>
2017-09-07 17:47:08 -0700

    questions load

--- js/index.js
+++ js/index.js
@@ -9,31 +9,48 @@ var allQuestions = [
 {
   question: "How many feet are there in a fathom?", 
   choices: ["4", "5", "6"], 
-  correctAnswerIndex:2
+  correctAnswerIndex:2,
+  name: "one"
 },
 {
   question: "What type of animal is a kolinsky?", 
   choices: ["weasel", "muskrat", "bear"], 
-  correctAnswerIndex:0
+  correctAnswerIndex:0,
+  name: "two"
 },
 {
   question: "Which of these colors does not appear on the Italian flag?", 
   choices: ["green", "red", "blue"], 
-  correctAnswerIndex:2
+  correctAnswerIndex:2,
+  name:"three"
 }
 ]; 
 
-for (var i = 0; i < allQuestions.length; i++) {
-  var label = document.createElement("LABEL");
-  label.append(allQuestions[i].question);
-  $('form').append(label);
-  for (var j = 0; j < allQuestions[i].choices[j]; j++) {
-    var input = document.createElement('INPUT')
-    input.attr({
-      "type":"radio",
-      "name": "q1",
-      });
-    input.val(allQuestions[i].choices[j]);
-    $('form').append(input);
+var score = 0; 
+var numberQuestions = 3; 
+
+
+function changeQuestion(){
+  for (var i = 0; i < allQuestions.length; i++) {
+      var label = document.createElement("p");
+      label.append(allQuestions[i].question);
+      $('form').append(label);
+
+      for (var j = 0; j < allQuestions[i].choices.length; j++) {
+        var input = $('<input type="radio" name='+allQuestions[i].name+'>');
+        var text = allQuestions[i].choices[j]; 
+        $(input).val(allQuestions[i].choices[j])
+        $('form').append(input);
+        $('form').append(text + '<br>');
+      }
+
   }
+
+  // if($('input[allQuestions[i].name]:checked').val() == allQuestions[i].choices[correctAnswerIndex]){
+// console.log(input[allQuestions[i].name]':checked');
+  // };
 }
+
+//if checked radio button's value = choices[correctAnswerIndex] then score ++
+// indexof checked radio button == allQuestions[i].correctAnswerIndex;
+
