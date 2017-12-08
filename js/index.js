158ebe7f0ad1663d99825c8c54ddcd6634b4a918
RAnderso0325 <reillyjanderson@gmail.com>
2017-12-07 16:31:12 -0800

    did some javascript

--- js/index.js
+++ js/index.js
@@ -1,5 +1,47 @@
 /*
-* Your names
+* Jacquelyn, Taylor, Reilly
 */
+var nextButton = document.getElementById("next-button");
 
-console.log("JS file is linked up!");
+var currentQ;
+
+var currentC = [];
+
+var quizArr = [
+{
+	question: "This is question 1",
+	choices: ["A", "B", "C", "D"],
+	correctAnswerIndex:0
+},
+{
+	question: "This is question 2",
+	choices: ["A", "B", "C", "D"],
+	correctAnswerIndex:1
+},
+{
+	question: "This is question 3",
+	choices: ["A", "B", "C", "D"],
+	correctAnswerIndex:2
+},
+{
+	question: "This is question 4",
+	choices: ["A", "B", "C", "D"],
+	correctAnswerIndex:3
+}];
+
+
+
+function nextQuestion(){
+	console.log(quizArr);
+}
+
+function updateScore(){
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+	nextQuestion();
+	nextButton.addEventListener("click", nextQuestion);
+	
+})
