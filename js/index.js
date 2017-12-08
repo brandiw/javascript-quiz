a4391701353970a0f0ecc636d96c5aa1b42938a3
JustinPMitchell <j.mitchell.real@gmail.com>
2017-12-08 01:57:58 -0800

    restyled

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var allQuestions = [{question: "Which of the following Wes Anderson film came first?",
-					 choices: ["The Life Aquatic", "The Royal Tenenbaums", "The Darjeeling Limited", "Fantastic Mr. Fox"],
+					 choices: ["The Life Aquatic", "The Royal Tenenbaums", "The Darturneeling Limited", "Fantastic Mr. Fox"],
 					 correctAnswerIndex:1},
 					 {question: "Which Noah Bambauch film did Wes Anderson Produce?",
 					 choices: ["Margot at the Wedding", "the Squid and the Whale", "The Meyerowitz Stories (New and Selected)", "Rushmore"],
@@ -13,100 +13,103 @@ var allQuestions = [{question: "Which of the following Wes Anderson film came fi
 					 {question: "What is the name of the most recent Wes Anderson film?",
 					 choices: ["Moonrise Kingdom", "Grand Budapest Hotel", "Hotel Chevalier", "Bottle Rocket"],
 					 correctAnswerIndex:1}];
-
-					 // [{question: "Who is Prime Minister of the United Kingdom?", 
-					 // choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
-					 // correctAnswerIndex:0},
-					 // {question: "Who is Prime Minister of the United Kingdom?2", 
-					 // choices: ["David Cameron2", "Gordon Brown2", "Winston Churchill2", "Tony Blair2"], 
-					 // correctAnswerIndex:1}
-var score = -1;
-var j = 1;
+var score = 0;
+var turn = 0;
+var submit = document.getElementsByClassName("submit")[0];
+selectionsBoolean = [];
 selections = [];
 
+//selections is now arry of booleans from the select buttons
 for(var i = 0; i < document.getElementsByClassName("selection").length; i++) {
-	selections.push(document.getElementsByClassName("selection")[i].checked);
+	selectionsBoolean.push(document.getElementsByClassName("selection")[i].checked);
 }
 
-console.log(selections);
-// var answers = [];
-// for(var i = 0; i < document.getElementsByClassName("answer").length; i++) {
-// 	answers[i] = document.getElementsByClassName("answer")[i].textContent;
-// }
+for(var i = 0; i < document.getElementsByClassName("selection").length; i++) {
+	selections.push(document.getElementsByClassName("selection")[i]);
+}
 
-
-// document.getElementsByClassName("answer")[0].textContent = allQuestions[1].choices[0];
+//removes event listeners
+var endGame = function() {
+	document.getElementsByClassName("question-box")[0].style.display = "none";
+	document.getElementsByClassName("scoreFrame")[0].style.display = "block";
+	document.getElementsByClassName("myScore")[0].textContent += score + " out of " + allQuestions.length + " correctly";
+	document.getElementsByClassName("percent")[0].textContent += (score / allQuestions.length * 100) + "%";
+	submit.removeEventListener("click", place);
+	selections[0].removeEventListener("click", function() {for(var i = 0; i < selectionsBoolean.length; i++) {selectionsBoolean[i] = false;}; selectionsBoolean[0] = true; console.log(selectionsBoolean);});
+	selections[1].removeEventListener("click", function() {for(var i = 0; i < selectionsBoolean.length; i++) {selectionsBoolean[i] = false;}; selectionsBoolean[1] = true; console.log(selectionsBoolean);});
+	selections[2].removeEventListener("click", function() {for(var i = 0; i < selectionsBoolean.length; i++) {selectionsBoolean[i] = false;}; selectionsBoolean[2] = true; console.log(selectionsBoolean);});
+	selections[3].removeEventListener("click", function() {for(var i = 0; i < selectionsBoolean.length; i++) {selectionsBoolean[i] = false;}; selectionsBoolean[3] = true; console.log(selectionsBoolean);});
+	console.log("end");
+}
 
 //function when submit switch question, tally score
  var tally = function() {
-	console.log(selections);
-	var selectedButton = selections.indexOf(true);
-	console.log("correct answer " + allQuestions[j].correctAnswerIndex + " selected answer " + (selectedButton += 1));
-	
-	if(allQuestions[j].correctAnswerIndex === selectedButton) {
-		score++;
+ 	if(turn != 0) {
+		console.log(selectionsBoolean);
+		var selectedButton = selectionsBoolean.indexOf(true);
+		console.log("correct answer " + allQuestions[turn-1].correctAnswerIndex + " selected answer " + (selectedButton));
+		
+		if(allQuestions[turn-1].correctAnswerIndex === selectedButton) {
+			score++;
+		}
+		console.log("score " + score);
 	}
-	console.log("score " + score);
+	//handles after submit button is clicked for the questions list length
+	if(turn === (allQuestions.length)) {
+		endGame();
+	};
 }	
 
 //function put allquestions into li and ul
 var place = function() {
-	var ele = document.getElementsByClassName("selection");
 	var check = 0;
-	console.log("first check is " + check);
 
-	for(var i=0;i<ele.length;i++) {
-	    if(ele[i].checked === false) {
+
+
+	//tells if nothing is selected
+	for(var i = 0; i < selections.length ; i++) {
+	    if(selections[i].checked === false) {
 	    	check++;
 	    	console.log("second check is " + check);
 	    }
 	    
 	}
-	if(check === ele.length && j != 1) {
+	if(check === selections.length && turn != 0) {
 		console.log("this is empty");
 		alert("this is wrong");
-		//j--;
-		//console.log("turn2 " + j);
 		return false;
 	}
 
 	tally();
 
 	//clears radio buttons
-	
-	for(var i=0;i<ele.length;i++)
-	    ele[i].checked = false;
+	for(var i = 0; i < selections.length; i++) {
+	    selections[i].checked = false;
+	}
 
-	document.getElementById("query").textContent = allQuestions[(j-1)].question;
-	document.getElementsByClassName("answer")[0].textContent = allQuestions[(j-1)].choices[0];
-	document.getElementsByClassName("answer")[1].textContent = allQuestions[(j-1)].choices[1];
-	document.getElementsByClassName("answer")[2].textContent = allQuestions[(j-1)].choices[2];
-	document.getElementsByClassName("answer")[3].textContent = allQuestions[(j-1)].choices[3];
-	j++;
-	console.log("turn " + j);
+	if(turn != allQuestions.length) {
+		document.getElementById("query").textContent = allQuestions[turn].question;
 
-	if(j === 5) {
-		document.getElementsByClassName("myScore")[0].style.display = "inline-block";
-		document.getElementsByClassName("myScore")[0].textContent += score;
+		for(var i = 0; i < selectionsBoolean.length; i++) {
+			document.getElementsByClassName("answer")[i].textContent = allQuestions[turn].choices[i];
+		}
+		turn++;
+		console.log("turn " + turn);
 	}
 }
 
 
 
-
-
-
-
-
-
 //when allQuestion.length go to complete screen
+submit.addEventListener("click", place);
+selections[0].addEventListener("click", function() {for(var i = 0; i < selectionsBoolean.length; i++) {selectionsBoolean[i] = false;}; selectionsBoolean[0] = true; console.log(selectionsBoolean);});
+selections[1].addEventListener("click", function() {for(var i = 0; i < selectionsBoolean.length; i++) {selectionsBoolean[i] = false;}; selectionsBoolean[1] = true; console.log(selectionsBoolean);});
+selections[2].addEventListener("click", function() {for(var i = 0; i < selectionsBoolean.length; i++) {selectionsBoolean[i] = false;}; selectionsBoolean[2] = true; console.log(selectionsBoolean);});
+selections[3].addEventListener("click", function() {for(var i = 0; i < selectionsBoolean.length; i++) {selectionsBoolean[i] = false;}; selectionsBoolean[3] = true; console.log(selectionsBoolean);});
 
-document.getElementsByClassName("submit")[0].addEventListener("click", place);
-document.getElementsByClassName("selection")[0].addEventListener("click", function() {for(var i = 0; i < selections.length; i++) {selections[i] = false;}; selections[0] = true; console.log(selections);});
-document.getElementsByClassName("selection")[1].addEventListener("click", function() {for(var i = 0; i < selections.length; i++) {selections[i] = false;}; selections[1] = true; console.log(selections);});
-document.getElementsByClassName("selection")[2].addEventListener("click", function() {for(var i = 0; i < selections.length; i++) {selections[i] = false;}; selections[2] = true; console.log(selections);});
-document.getElementsByClassName("selection")[3].addEventListener("click", function() {for(var i = 0; i < selections.length; i++) {selections[i] = false;}; selections[3] = true; console.log(selections);});
-// for(var k = 0; k < document.getElementsByClassName("selection").length; k++) {
-// 	document.getElementsByClassName("selection")[k].addEventListener("click", function() {selections[k] = true; console.log(selections);});
-// }
+
 place();
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
