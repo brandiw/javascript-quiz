c269c647e5ae27e77586973e68ec3b3905cf88c3
rallysport206 <phan.tony125@gmail.com>
2017-12-07 20:30:54 -0800

    questions added to quiz

--- js/questions.js
+++ js/questions.js
@@ -1,12 +1,18 @@
-var questions =	
+var questions =
 [
-	{question: "Who is Prime Minister of the United Kingdom?", 
-	choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+	{question: "Who was the final act at Woodstock?",
+	choices: ["Grateful Dead", "Joe Cocker", "Jimi Hendrix", "Janis Joplin"],
+	correctAnswerIndex:2},
+	{question: "Blackstar was whose last album, released just days before his death?",
+	choices: ["Leonard Cohen", "George Michael", "Prince", "David Bowie", "Glenn Frey"],
+	correctAnswerIndex:3},
+	{question: "With which group would you associate the songs \"Light My Fighter\"and \"Hello I Love You\"?",
+	choices: ["The Doors", "Pink Floyd", "The Beatles", "Eagles", "Led Zeppelin"],
 	correctAnswerIndex:0},
-	{question: "NEXT", 
-	choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
-	correctAnswerIndex:0},
-	{question: "LAST", 
-	choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+	{question: "Who joined Alicia Keys on the 2002 hit song \"Gangsta Lovin\"?",
+	choices: ["Jay Z", "Jennifer Hudson", "Swizz Beatz", "Beyonce", "Eve"],
+	correctAnswerIndex:4},
+	{question: "With which group would you associate the songs \"Light My Fighter\"and \"Hello I Love You\"?",
+	choices: ["The Doors", "Pink Floyd", "The Beatles", "Eagles", "Led Zeppelin"],
 	correctAnswerIndex:0}
-]; 
+];
