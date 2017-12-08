a9112620e700fd6479016cbe29e11919415ec45c
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2017-12-07 17:17:55 -0800

    working quiz - next step score counter

--- js/index.js
+++ js/index.js
@@ -3,3 +3,78 @@
 */
 
 console.log("JS file is linked up!");
+
+
+
+// Global Variables 
+var questionNum;
+var answers = [];
+
+// Functions 
+
+// Initialize Page
+function startQuiz() {
+	questionNum = 0;
+	
+	showQuestion(questionNum);
+}
+
+// To go to the next question
+function nextQuestion() {
+	if(questionNum === questions.length -1){
+		// END QUIZ
+	}
+	questionNum++;
+	showQuestion(questionNum);
+}
+
+// To calculate score
+function scoreCounter() {
+
+}
+
+// To show questions
+
+function showQuestion(x){
+
+	document.getElementById("questionText").innerHTML = questions[x].question;
+
+	var choices = questions[x].choices;
+	console.log(choices);
+
+	for(var i =0; i < choices.length; i ++) {
+		var label = "answer" + i;
+		// console.log(label);
+		document.getElementById(label).innerHTML = choices[i];
+	}
+
+	for(var i = choices.length; i <= 4; i++){
+		var container = "container" + i;
+		document.getElementById(container).style.visibility = 'hidden';
+	}
+
+}
+
+// To store answer
+function getAnswer() {
+
+	var choices = questions[questionNum].choices;
+	var test = document.getElementById("answerForm").querySelectorAll("input");
+	for(var i = 0; i < test.length; i++){
+		console.log(test[i]);
+		if(test[i].checked === true){
+			answers.push(i);
+		}
+	}
+	console.log(answers);
+	nextQuestion();
+
+}
+
+// To show results
+function endQuiz() {
+
+}
+
+var next = document.getElementById('nextQ').addEventListener("click",getAnswer);
+startQuiz();
---
+++ js/questions.js
@@ -0,0 +1,12 @@
+var questions =	
+[
+	{question: "Who is Prime Minister of the United Kingdom?", 
+	choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+	correctAnswerIndex:0},
+	{question: "NEXT", 
+	choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+	correctAnswerIndex:0},
+	{question: "LAST", 
+	choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
+	correctAnswerIndex:0}
+]; 
