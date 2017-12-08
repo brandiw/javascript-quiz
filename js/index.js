635b507ad693292e63df2ca1b14cd38416375f0d
Nick_edu <nick_edu@nameless.local>
2017-12-07 18:48:13 -0800

    Adds read radio output fx

--- js/index.js
+++ js/index.js
@@ -36,15 +36,20 @@ $(function() {
   var currentQuestion = 0;
   var questionsHtml = [];
 
+
+
+  // takes an object containg questions and generate an array containing the 
+  //HTML for each question
+
   function generateBoard() {
 
-    
+
     var questionHtml = null;
     for (let i = 0; i < QUESTIONS.length; i += 1) {
 
-      questionHtml = 
-      
-      `
+      questionHtml =
+
+        `
       <div class="row">
         <div class="col-sm-3 col-xs-12"></div>
         <div class="col-sm-6 col-xs-12">
@@ -56,35 +61,36 @@ $(function() {
         <div class="col-sm-3 col-xs-12"></div>
         <div class="col-sm-6 col-xs-12">
             <ul>
-              <li id="ans1">${QUESTIONS[i].answers.a}</li>
-              <li id="ans2">${QUESTIONS[i].answers.b}</li>
-              <li id="ans3">${QUESTIONS[i].answers.c}</li>
-              <li id="ans4">${QUESTIONS[i].answers.d}</li>
+            <div class="radio"><label><input type="radio" name="ansRadios" id="radioAns1">${QUESTIONS[i].answers.a}</label></div>
+            <div class="radio"><label><input type="radio" name="ansRadios" id="radioAns2">${QUESTIONS[i].answers.b}</label></div>
+            <div class="radio"><label><input type="radio" name="ansRadios" id="radioAns3">${QUESTIONS[i].answers.c}</label></div>
+            <div class="radio"><label><input type="radio" name="ansRadios" id="radioAns4">${QUESTIONS[i].answers.d}</label></div>
             </ul>
           </div>
           <div class="col-sm-3 col-xs-12"></div>
         </div>
-        <div class="row">
+        <div class="row buttons">
         <div class="col-xs-6"></div>
         <div class="col-xs-6">
-          <button id="next">Next Question</button>
+          <button id="next" class="btn btn-primary">Next Question</button>
         </div>
       </div>
       `;
-          questionsHtml.push(questionHtml);
-     //     console.log(questionHtml);
+      questionsHtml.push(questionHtml);
+      //     console.log(questionHtml);
     }
-    
+
     // $('#quizBox').append(questionsHtml);
 
   }
 
   // displays title screen and a start button
-
+  //takes in the current question number; 
+  //empties the div containg the previous question
+  //appends the newly generated question from a questionsHtml array 
   function changeQuestion(currentQuestion) {
 
-
-    console.log('current question: ',currentQuestion);
+    console.log('current question: ', currentQuestion);
 
     $('#quizBox').empty();
 
@@ -92,18 +98,18 @@ $(function() {
 
     init();
   }
-  
-function checkResult(input, questionNumber){
-  if (input=== QUESTIONS[questionNumber].correctAnswer) {
-    return true;
-  }
-}
 
-function doneWithTest(input){
-  if (currentQuestion===QUESTIONS.length){
-    alert('Done');
+  function checkResult(input, questionNumber) {
+    if (input === QUESTIONS[questionNumber].correctAnswer) {
+      return true;
+    }
   }
-}
+
+  function doneWithTest(input) {
+    if (currentQuestion === QUESTIONS.length) {
+      alert('Done');
+    }
+  }
   //cycle through to next question in questions
   //changes the values of the question title to current quetion
   //changes the values of the radio box inputs
@@ -118,12 +124,15 @@ function doneWithTest(input){
     //add value to new array?
   }
 
+  // runs when page loaded
   function init() {
     console.log('before done', currentQuestion);
     console.log(doneWithTest(currentQuestion));
 
+    // generates the board from start screen
     $('#button').on('click', generateBoard);
 
+    //increments the questions by 1 and changes call change questions
     $('#next').on('click', () => {
 
       currentQuestion += 1;
@@ -134,6 +143,29 @@ function doneWithTest(input){
     generateBoard();
 
   }
+    $("input[type='radio']").click(function(event) {
+      var radioValue = $("input[name='ansRadios']:checked").val();
+      if (radioValue) {
+        console.log(event.target.attributes[2].value);
+      }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   init();
 
