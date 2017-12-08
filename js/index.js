c76df18fef82dd998f762c857eb7b7b712176e00
Nick_edu <nick_edu@nameless.local>
2017-12-07 17:52:08 -0800

    Generates board

--- js/index.js
+++ js/index.js
@@ -1,63 +1,82 @@
 $(function() {
 
-  const questions = [
-    {
+  const QUESTIONS = [{
       question: 'Question 1',
-      answers: [{
+      answers: {
         a: 'a',
         b: 'b',
         c: 'c',
         d: 'd'
-      }],
+      },
       correctAnswer: 'a'
     },
     {
       question: 'Question 2',
-      answers: [{
+      answers: {
         a: 'a',
         b: 'b',
         c: 'c',
         d: 'd'
-      }],
+      },
       correctAnswer: 'b'
     }
   ]
 
 
-  var currentQuestion =0 ;
+  var currentQuestion = 0;
 
-function generateBoard(){
-}
+  function generateBoard() {
 
-// displays title screen and a start button
+    var questionsHtml = []
+    var questionHtml = null;
+    for (let i = 0; i < QUESTIONS.length; i += 1) {
 
-function changeQuestion(questions, currentQuestion){}
+      questionHtml = `<div class="col-xs-12">
+          <h1>${QUESTIONS[i].question}</h1>
+        </div>        
+        <div class="col-xs-12">
+            <ul>
+              <li id="ans1">${QUESTIONS[i].answers.a}</li>
+              <li id="ans2">${QUESTIONS[i].answers.b}</li>
+              <li id="ans3">${QUESTIONS[i].answers.c}</li>
+              <li id="ans4">${QUESTIONS[i].answers.d}</li>
+            </ul>
+          </div>`;
+          questionsHtml.push(questionHtml);
+          console.log(questionHtml);
+    }
+    
+     $('#quizBox').append(questionsHtml);
 
-//changes the values of the question title to current quetion
-//changes the values of the radio box inputs
+  }
+
+  // displays title screen and a start button
+
+  function changeQuestion(questions, currentQuestion) {}
+
+  //changes the values of the question title to current quetion
+  //changes the values of the radio box inputs
 
 
-function readInput(){
+  function readInput() {
 
-  //reads the value of the clicked radio button
-}
+    //reads the value of the clicked radio button
+    //returns said value
+  }
 
-function init(){
+  function init() {
+    $('#button').on('click', generateBoard);
 
-  $('#next').on('click', ()=>{
+    $('#next').on('click', () => {
+      currentQuestion += 1;
+      changeQuestion(questions, currentQuestion);
+    })
 
-    currentQuestion +=1;
+    generateBoard(QUESTIONS);
 
+  }
 
-    changeQuestion(questions, currentQuestion);
-
-  })
-  
-  generateBoard(questions);
-
-}
-
-init();
+  init();
 
 
 
