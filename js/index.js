ea732a51d655a0c21105ca05b5f88b12c0029956
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2017-12-07 18:08:25 -0800

    working quiz with back button and score scounter

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@
 
 console.log("JS file is linked up!");
 
-
-
 // Global Variables 
 var questionNum;
 var answers = [];
@@ -15,22 +13,59 @@ var answers = [];
 // Initialize Page
 function startQuiz() {
 	questionNum = 0;
+	document.getElementById("answers").style.visibility = 'visible';
+	document.getElementById("previousQ").style.visibility = 'visible';
+	document.getElementById("nextQ").style.visibility = 'visible';
 	
+	var answers = new Array(questions.length);
+
 	showQuestion(questionNum);
 }
 
 // To go to the next question
 function nextQuestion() {
-	if(questionNum === questions.length -1){
-		// END QUIZ
+	if(questionNum === questions.length-1){
+		endQuiz();
+	} else {
+		questionNum++;
+		showQuestion(questionNum);
+		updateAnswer();
 	}
-	questionNum++;
-	showQuestion(questionNum);
+}
+
+// To go to the previous question
+function previousQuestion() {
+
+	if(questionNum === 0){
+		return;
+	} else {
+		questionNum--;
+		showQuestion(questionNum);
+		updateAnswer();
+	}
+}
+
+function updateAnswer(){
+	var test = document.getElementById("answerForm").querySelectorAll("input");
+	for(var i = 0; i < test.length; i++){
+		if(i === answers[questionNum]){
+			test[i].checked = true;
+		} else {
+			test[i].checked = false;
+		}
+	}
 }
 
 // To calculate score
 function scoreCounter() {
+	var score = 0;
 
+	for(var i = 0; i < questions.length; i++) {
+		if (answers[i] === questions[i].correctAnswerIndex){
+			score++;
+		}
+	}
+	return score;
 }
 
 // To show questions
@@ -63,18 +98,24 @@ function getAnswer() {
 	for(var i = 0; i < test.length; i++){
 		console.log(test[i]);
 		if(test[i].checked === true){
-			answers.push(i);
+			answers[questionNum] = i;
 		}
 	}
-	console.log(answers);
 	nextQuestion();
 
 }
 
 // To show results
 function endQuiz() {
+	document.getElementById("answers").style.visibility = 'hidden';
+	document.getElementById("previousQ").style.visibility = 'hidden';
+	document.getElementById("nextQ").style.visibility = 'hidden';
+	document.getElementById("questionText").innerHTML = "You're done!"
+	document.getElementById("results").innerHTML = "You scored: " +scoreCounter();
+
 
 }
 
 var next = document.getElementById('nextQ').addEventListener("click",getAnswer);
+var prev = document.getElementById('previousQ').addEventListener("click",previousQuestion);
 startQuiz();
