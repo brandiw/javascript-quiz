b7fc2897712a462a6505215e68ee60808b47f10f
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-08-01 14:33:45 -0700

    created HTML and basic CSS, created array with questions. no logic

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("JS file is linked up!");
+
+//need array with 4 questions and correct answers 
+//need logic to grab submission from
+
+
+
+
+var allQuestions = [{
+	question: "This is question 1", 
+	choices: ["question 1 answer 1", "question 1 answer 2", "question 1 answer 3", "question 1 answer 4"], 
+	correctAnswerIndex:0
+	}{
+	question: "This is question 2", 
+	choices: ["question 2 answer 1", "question 2 answer 2", "question 2 answer 3", "question 2 answer 4"], 
+	correctAnswerIndex:0
+	}{
+	question: "This is question 3", 
+	choices: ["question 3 answer 1", "question 3 answer 2", "question 3 answer 3", "question 3 answer 4"], 
+	correctAnswerIndex:0
+	}{
+	question: "This is question 4", 
+	choices: ["question 4 answer 1", "question 4 answer 2", "question 4 answer 3", "question 4 answer 4"], 
+	correctAnswerIndex:0
+}]; 
